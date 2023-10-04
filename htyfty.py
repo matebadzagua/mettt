8d8edf16126754cdd3a8e38c8038a7b005506c5f
matebadzagua <146456473+matebadzagua@users.noreply.github.com>
2023-10-04 07:46:07 -0700

    Add files via upload

---
+++ htyfty.py
@@ -0,0 +1,8 @@
+user_age = input("what's your age:?  l am 33 years old " )
+
+
+print("as user said, he is ", user_age )
+
+user_name = input("what your name:? my name is mate")
+
+print("as user said, he is", user_name) 
